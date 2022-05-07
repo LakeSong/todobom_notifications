855acc5ee4653250d17cad00411ef78bb0d05953
Shir Agami <shirag2007@gmail.com>
2022-05-07 19:21:13 +0300

    added env port

--- app/index.js
+++ app/index.js
@@ -14,7 +14,7 @@ import {
 import express from "express";
 import cors from 'cors'
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: function (origin, callback) {
