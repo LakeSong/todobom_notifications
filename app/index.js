c3b282f12de43541bd75ce7c6b3f0f80e1b451d2
Shir Agami <shirag2007@gmail.com>
2022-06-08 17:00:40 +0300

    bug fix

--- app/index.js
+++ app/index.js
@@ -12,26 +12,27 @@ import {
 } from "./services/jobService.js";
 
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: function (origin, callback) {
-      callback(null, true);
+    callback(null, true);
   },
   credentials: true,
-}
-app.use(cors(corsOptions))
+};
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.json(Object.keys(getScheduledJobs()).length);
 });
 
-app.delete("/:id",async (req, res) => {
-   await cancelJob(`${req.params.id}`);
-   res.sendStatus(200);
-})
+app.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  id && (await cancelJob(`${id}`));
+  res.sendStatus(200);
+});
 
 app.listen(port, async () => {
   console.log(`Server listening on port ${port}`);
